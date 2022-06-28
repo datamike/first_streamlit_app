c03f44b799ed489eed2a2d25cfacdeb1a11821dc
Michael Boyle <boyle.md07@gmail.com>
2022-06-28 10:11:24 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to display Fruityvice
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 # streamlit.text(fruityvice_response.json())
 
 # Normalize the data into a more readable table format 
